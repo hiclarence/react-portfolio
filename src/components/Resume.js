176053570beaaa646d6ead9029c89f10265f8c59
clarence_c <clarence@biteline.net>
2023-06-17 14:50:10 -0700

    add skills

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,11 +4,24 @@ import resume from "../assets/resume.pdf"
 export default function Resume() {
   return (
     <div className="resume">
-        <div>
+        <div classname="skillsTitle">
           <h2>My Skills</h2>
         </div>
-        <div>
-          <ul>My Skills</ul>
+        <div className="skillSection">
+          <div className="skillset col_1">
+            <ul>HTML/CSS</ul>
+            <ul>Javascript</ul>
+            <ul>Express</ul>
+            <ul>Node</ul>
+            <ul>React</ul>
+          </div>
+          <div className="skillset col_2">
+            <ul>Handlebars</ul>
+            <ul>SQL</ul>
+            <ul>MongoDB</ul>
+            <ul>MERN</ul>
+            <ul>GraphQL</ul>  
+          </div>
         </div>
         <a href={resume} download><button type="button">Download my Resume</button></a>
     </div>
